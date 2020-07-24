a9d01430cc8e8cb5cbb45e5c0d18f1e0c85be948
Marat Gumerov <gumerovm74@gmail.com>
2020-07-24 10:06:51 +0300

    add dropdown

--- src/pages/form-elements/form-elements.js
+++ src/pages/form-elements/form-elements.js
@@ -1,1 +1,34 @@
+// Styles
 import './form-elements';
+
+
+import { dropdown } from '../../components/dropdown/dropdown.js';
+
+
+var readyPage = (callback) => {
+  if (document.readyState != "loading") callback();
+  else document.addEventListener("DOMContentLoaded", callback);
+}
+
+
+readyPage(() => {
+  //const maskedTextDate = new MaskedTextField('date')
+
+  const demoDropDownVisitors = new dropdown('.dropdown-visitors');
+  const demoDropDownVisitorsWithoutClear = new dropdown('.dropdown-visitors-without-clear');
+  const demoDropDownVisitorsClear = new dropdown('.dropdown-visitors-clear');
+
+  const demoDropDownEnarenmentClose = new dropdown('.dropdown-envarenment-close');
+  const demoDropDownEnarenmentOpen = new dropdown('.dropdown-envarenment-open');
+});
+
+// изображения
+// import '../../modules/comment/img/avatar-free.png'
+// import './images/avatar.png'
+// import '../../modules/logo/images/logo.svg'
+// import '../../modules/logo/images/logo-gray.svg'
+// import '../../modules/logo/images/logo-mobile.svg'
+
+
+// собственные модули
+// import '../../modules/field-checkbox/field-checkbox'
